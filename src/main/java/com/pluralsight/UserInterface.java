84d2b63b347ac387cf362a0e5dfd8bf39c3a8cb9
branRoberts <brandonkrob12@gmail.com>
2026-05-13 21:05:23 -0400

    Eighth Commit added code to the methods asking the user to search for a vehicles using a specific field and later will add methods that use their inputs

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 
 public class UserInterface {
     private Dealership dealership;
+    Scanner input = new Scanner(System.in);
     private void init(){
        DealershipFileManager dealershipFileManager = new DealershipFileManager();
        dealership = dealershipFileManager.getDealership();
     }
 
     public void display(){
-        Scanner input = new Scanner(System.in);
+
         init();
         while(true){
             System.out.println("Welcome to the Vehicle Menu");
@@ -51,12 +52,45 @@ public class UserInterface {
             System.out.println(vehicle);
         }
     }
-    public void processPriceRangeRequest(){}
-    public void processMakeModelRequest(){}
-    public void processYearRangeRequest(){}
-    public void processColorRequest(){}
-    public void processMileageRangeRequest(){}
-    public void processVehicleTypeRequest(){}
+    public void processPriceRangeRequest(){
+        System.out.println("Please enter the Min price range for this vehicle");
+        double minPrice = Double.parseDouble(input.nextLine());
+        System.out.println("Please enter the Max price range for this vehicle");
+        double maxPrice = Double.parseDouble(input.nextLine());
+        displayVehicles(dealership.getVehiclesByPrice(minPrice, maxPrice));
+    }
+    public void processMakeModelRequest(){
+        System.out.println("Please enter the Make for this vehicle");
+        String vehicleMake = input.nextLine();
+        System.out.println("Please enter the Model for this vehicle");
+        String vehicleModel = input.nextLine();
+        displayVehicles(dealership.getVehiclesByMakeModel(vehicleMake,vehicleModel));
+
+    }
+    public void processYearRangeRequest(){
+        System.out.println("Please enter the Min Year for this vehicle");
+        int minYear = Integer.parseInt(input.nextLine());
+        System.out.println("Please enter the Max Year for this vehicle");
+        int maxYear = Integer.parseInt(input.nextLine());
+        displayVehicles(dealership.getVehiclesByYearRange(minYear,maxYear));
+    }
+    public void processColorRequest(){
+        System.out.println("Please enter the Color for this vehicle");
+        String vehicleColor = input.nextLine();
+        displayVehicles(dealership.getVehiclesByColor(vehicleColor));
+    }
+    public void processMileageRangeRequest(){
+        System.out.println("Please enter the Min Mileage for this vehicle");
+        int minMileage = Integer.parseInt(input.nextLine());
+        System.out.println("Please enter the Max Mileage for this vehicle");
+        int maxMileage = Integer.parseInt(input.nextLine());
+        displayVehicles(dealership.getVehiclesByMileageRange(minMileage,maxMileage));
+    }
+    public void processVehicleTypeRequest(){
+        System.out.println("Please enter the Vehicle Type for this vehicle");
+        String vehicleType = input.nextLine();
+        displayVehicles(dealership.getVehiclesByvehicleType(vehicleType));
+    }
     public void processAllVehiclesRequest(){
         displayVehicles(dealership.getAllVehicles());
     }
